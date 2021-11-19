cc8f57ed4c15809d15da550023a207c4783def83
lsw-56399 <92102761+lsw-56399@users.noreply.github.com>
2021-11-19 14:06:12 +0700

    Create tableloop.php

---
+++ tableloop.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+  </head>
+  <body>
+    table border="1">
+      <tr>
+        <td>คอลัมน์ 1 แถว 1</td>
+        <td>คอลัมน์ 2 แถว 1</td>
+      </tr>
+      <tr>
+        <td>คอลัมน์ 1 แถว 2</td>
+        <td>คอลัมน์ 2 แถว 2</td>
+      </tr>
+    </table>
+  </body>
+</html>
+นายยิ่งคุณ รอดทิม1:54 PM
+https://lsw-เลขประจำตัว.herokuapp.com/tableloop.php
+นายยิ่งคุณ รอดทิม1:57 PM
+<html>
+  <head>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <td>คอลัมน์ 1 แถว 1</td>
+        <td>คอลัมน์ 2 แถว 1</td>
+      </tr>
+      <tr>
+        <td>คอลัมน์ 1 แถว 2</td>
+        <td>คอลัมน์ 2 แถว 2</td>
+      </tr>
+    </table>
+  </body>
+</html>
