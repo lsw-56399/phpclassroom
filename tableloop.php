0274422d04bfe0cbfc23d1f400cc011e7455c80c
lsw-56399 <92102761+lsw-56399@users.noreply.github.com>
2021-11-19 15:59:55 +0700

    Update tableloop.php

--- tableloop.php
+++ tableloop.php
@@ -3,13 +3,19 @@
   </head>
   <body>
     <table border="1">
-      <?php
-        for($i<6;$i<100;$i++){
+       <?php
+      for($i=0;$i<100;$i++){
           echo "<tr>";
-          echo "<td>เจมส์</td>";
+          echo "<td>คอลัมน์1</td>";
+          echo "<td>คอลัมน์2</td>";
+          echo "<td>คอลัมน์3</td>";
+          echo "<td>คอลัมน์4</td>";
+          echo "<td>คอลัมน์5</td>";
+          echo "<td>คอลัมน์6</td>";
+          
           echo "</tr>";
         }
-      ?>
+       ?>
     </table>
   </body>
 </html>
