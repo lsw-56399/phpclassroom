a47333ae66c359f498e9d1010a1352b765d9d833
lsw-56399 <92102761+lsw-56399@users.noreply.github.com>
2021-11-19 14:09:33 +0700

    Update tableloop.php

--- tableloop.php
+++ tableloop.php
@@ -1,35 +1,15 @@
-<html>
-  <head>
-  </head>
-  <body>
-    table border="1">
-      <tr>
-        <td>คอลัมน์ 1 แถว 1</td>
-        <td>คอลัมน์ 2 แถว 1</td>
-      </tr>
-      <tr>
-        <td>คอลัมน์ 1 แถว 2</td>
-        <td>คอลัมน์ 2 แถว 2</td>
-      </tr>
-    </table>
-  </body>
-</html>
-นายยิ่งคุณ รอดทิม1:54 PM
-https://lsw-เลขประจำตัว.herokuapp.com/tableloop.php
-นายยิ่งคุณ รอดทิม1:57 PM
-<html>
+<<html>
   <head>
   </head>
   <body>
     <table border="1">
-      <tr>
-        <td>คอลัมน์ 1 แถว 1</td>
-        <td>คอลัมน์ 2 แถว 1</td>
-      </tr>
-      <tr>
-        <td>คอลัมน์ 1 แถว 2</td>
-        <td>คอลัมน์ 2 แถว 2</td>
-      </tr>
+      <?php
+        for($i=0;$i<5;$i++){
+          echo "<tr>";
+          echo "<td>คอลัมน์</td>"
+          echo "</tr>";
+        }
+      ?>
     </table>
   </body>
 </html>
